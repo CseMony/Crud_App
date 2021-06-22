82ea3a365da1e1c9a7226f5b9647ad7b46e39e1c
mony <mony.bd@asa-international.com>
2021-06-22 13:38:11 +0600

    Unit Test Project added

---
+++ Crud_App/TestClass1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Crud_App
+{
+    public class TestClass1
+    {
+        public String Name;
+        public TestClass1()
+        {
+            Name = "Unit Testing";
+        }
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using Crud_App;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            
+            TestClass1 tp = new TestClass1();
+            Assert.AreEqual(tp.Name, "Unit Testing");
+        }
+    }
+}
